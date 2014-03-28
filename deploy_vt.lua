831730e8d5cee78c492866e8c0a8339bf70a08f4
mwalecki <mw@mwalecki.pl>
2014-03-28 17:01:27 +0100

    Head and torso control

--- deploy_vt.lua
+++ deploy_vt.lua
@@ -12,6 +12,7 @@ d:import("rtt_roscomm")
 ros=rtt.provides("ros")
 
 ros:import("velma_hardware")
+ros:import("controller_common")
 --ros:import("oro_joint_state_publisher")
 -- End of user code
 
@@ -42,14 +43,36 @@ HT = d:getPeer("HT")
 
 HT:configure()
 
+d:loadComponent("HJ", "HeadTeleopJoy")
+d:setActivity("HJ", 0.001, 6, rtt.globals.ORO_SCHED_RT)
+HT = d:getPeer("HJ")
+
+HT:configure()
+
 -------------------------------------------------------------------------------
 -- Connections and Triggers
 -------------------------------------------------------------------------------
 
-d:connect("VT.JointPositionCommand", "HT.JointPositionCommand", rtt.Variable("ConnPolicy"))
+-- -- VT.JointPositionCommand is a vector of size 3.
+--d:connect("VT.JointPositionCommand", "HT.JointPositionCommand", rtt.Variable("ConnPolicy"))
+
+d:loadComponent("Jc", "VectorConcate2")
+d:setActivity("Jc", 0.001, 2, rtt.globals.ORO_SCHED_RT)
+Jc = d:getPeer("Jc")
+d:connect("HT.FakeJointPositionCommand", "Jc.In0", rtt.Variable("ConnPolicy"))
+d:connect("HT.JointPositionCommand", "Jc.In1", rtt.Variable("ConnPolicy"))
+d:connect("HJ.FakeJointPositionCommand", "Jc.In0", rtt.Variable("ConnPolicy"))
+d:connect("HJ.JointPositionCommand", "Jc.In1", rtt.Variable("ConnPolicy"))
+d:connect("Jc.Out", "VT.JointPositionCommand", rtt.Variable("ConnPolicy"))
+Jc:configure()
+
+conn2ros(d, "HJ.Joy", "/joy")
+
+Jc:start()
 
 VT:start()
-HT:start()
+--HT:start()
+--HJ:start()
 
 
 -------------------------------------------------------------------------------
--- src/torso.cpp
+++ src/torso.cpp
@@ -8,6 +8,8 @@
 #include "MotorController.h"
 
 #define TORSO_MNJ 4
+#define POS_CTRL_MNJ 3
+#define TRQ_CTRL_MNJ 1
 
 #define OFFSET0 0
 #define OFFSET1 280
@@ -23,6 +25,11 @@
 #define ENC1 5000.0
 #define ENC2 500.0
 #define ENC3 500.0
+
+//#define MAX_VEL0
+#define MAX_VEL1 250
+#define MAX_VEL2 50000
+#define MAX_VEL3 20000
 
 class VelmaTorso : public RTT::TaskContext {
 public:
@@ -43,8 +50,8 @@ public:
 	bool configureHook() {
 		jnt_pos_.resize(TORSO_MNJ);
 		jnt_vel_.resize(TORSO_MNJ);
-		jnt_pos_cmd_.resize(TORSO_MNJ);
-		jnt_trq_cmd_.resize(TORSO_MNJ);
+		jnt_pos_cmd_.resize(POS_CTRL_MNJ);
+		jnt_trq_cmd_.resize(TRQ_CTRL_MNJ);
 
 		port_JointPosition.setDataSample(jnt_pos_);
 		port_JointVelocity.setDataSample(jnt_vel_);
@@ -62,6 +69,9 @@ public:
 		mc.setMode(1, MODE_POSITION);
 		mc.setMode(2, MODE_POSITION);
 		mc.setMode(3, MODE_POSITION);
+		mc.setVelocity(1, MAX_VEL1);
+		mc.setVelocity(2, MAX_VEL2);
+		mc.setVelocity(3, MAX_VEL3);
 		return true;
 	}
 
@@ -82,34 +92,30 @@ public:
 			// Read input ports
 			pos_fs = port_JointPositionCommand.read(jnt_pos_cmd_);
 			trq_fs = port_JointTorqueCommand.read(jnt_trq_cmd_);
-
-			/*
-			// Test of 2,3 drives control
-			if(++loopCnt > 5000){
-				loopCnt = 0;
-				testMoveDir = (testMoveDir == 10000) ? (-10000) : 10000;
-				mc.setPosition(2, testMoveDir);
-				mc.setVelocity(2, 10000);
-			}*/
 			
-			// Command new position values
+			
 			if(pos_fs == RTT::NewData){
-				std::cout<<"jnt_pos_cmd_ "<<jnt_pos_cmd_(0)<<" "<<jnt_pos_cmd_(1)<<" "<<jnt_pos_cmd_(2)<<" "<<jnt_pos_cmd_(3)<< std::endl;
-				mc.setPosition(2, jnt_pos_cmd_(2));
-				mc.setVelocity(2, 10000);
-				mc.setPosition(3, jnt_pos_cmd_(3));
-				mc.setVelocity(3, 10000);
+				// New position data: make a new head position command
+				//std::cout<<"jnt_pos_cmd_ "<<jnt_pos_cmd_(0)<<" "<<jnt_pos_cmd_(1)<<" "<<jnt_pos_cmd_(2)<< std::endl;
+				//mc.setPosition(2, jnt_pos_cmd_(2));
+				//mc.setPosition(3, jnt_pos_cmd_(3));
+				// Send data from JointPositionCommand port #1 and #2 to drives #2 and #3 (group #1)
+				mc.setPosition2(1, jnt_pos_cmd_(1), jnt_pos_cmd_(2));
 			}
 
-			int16_t trq0 = -((jnt_trq_cmd_(0)/GEAR0)/0.105) * 1.2 * 1000;
-
-			if(trq0 < 0.0) {
-				trq0 -= 500;
-			} else {
-				trq0 += 500;
+			if(pos_fs == RTT::NewData){
+				// New torque data: make a new torso torque and position command
+				int16_t trq0 = -((jnt_trq_cmd_(0)/GEAR0)/0.105) * 1.2 * 1000;
+				if(trq0 < 0.0) {
+					trq0 -= 500;
+				} else {
+					trq0 += 500;
+				}
+				// Send data computed from JointTorqueCommand port #0 to drive #0
+				mc.setCurrent(0, trq0);
+				// Send data from JointPositionCommand port #0 to drive #1
+				mc.setPosition(1, jnt_pos_cmd_(0));
 			}
-
-			mc.setCurrent(0, trq0);
 
 			mc.getPosition4(0, pos0, pos1, pos2, pos3);
 
